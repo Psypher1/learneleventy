22ff97d5bf2ae241fbbb62633072ef69f9532764
James 'Dante' Midzi <jamesmidzi@gmail.com>
2022-05-31 22:21:41 +0200

    shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,13 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/css/style.css");
   eleventyConfig.addPassthroughCopy("src/assets/images");
 
+  eleventyConfig.addShortcode(
+    "headers",
+    (title, subtitle) =>
+      `<h1>${title}</h1>
+        <p>${subtitle}</p>`
+  );
+
   return {
     dir: {
       input: "src",
