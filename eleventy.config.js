a32fb3845913611b8b709a2e3773bd4248cb0203
Dante <jamesmidzi@gmail.com>
2026-09-14 16:27:01 +0200

    feat: plugins

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,9 @@
+import { DateTime } from "luxon";
+
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/css/style.css");
   eleventyConfig.addPassthroughCopy("src/assets/images");
+  eleventyConfig.addPassthroughCopy({ "src/robots.txt": "/robots.txt" });
 
   eleventyConfig.addShortcode(
     "headers",
@@ -13,6 +16,14 @@ export default function (eleventyConfig) {
     return collections.getFilteredByTag("page").sort(function (a, b) {
       return a.data.order - b.data.order;
     });
+  });
+
+  eleventyConfig.addShortcode("currentDate", (date = DateTime.now()) => {
+    return date;
+  });
+
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
 
   return {
